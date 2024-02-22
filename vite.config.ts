ca8a9fc6951795debe0271553bcedace9164fe82
calm1205 <naginani072@gmail.com>
2024-02-22 21:46:20 +0900

    urlを活用するためにtypes/nodeのinstall

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import { fileURLToPath } from "node:url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +18,7 @@ export default defineConfig({
   resolve: {
     alias: {
       vue: "vue/dist/vue.esm-bundler.js",
-      "@": "./src",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   css: {
